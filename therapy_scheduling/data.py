eaf42d4cc6bd07c3a0e05979ade04872b819f9a6
widal001 <williamdaly422@gmail.com>
2021-02-14 21:51:36 -0500

    Adds valid schedule to data for testing

--- therapy_scheduling/data.py
+++ therapy_scheduling/data.py
@@ -9,6 +9,49 @@ INPUTS = {
         "staff": {"Therapy": ["Alice", "Bob"]},
         "groups": {"Therapy": ["Body Image"]},
         "individual": {"Therapy": 1},
-        "patients": 3,
+        "patients": ["Dana", "Eddie", "Francine"],
+    },
+}
+
+SCHEDULES = {
+    "one_category": {
+        "Morning": {
+            "Room 1": {
+                "type": "group",
+                "group": "Body Image",
+                "staff": "Alice",
+                "patients": ["Dana", "Eddie"],
+            },
+            "Room 2": {
+                "type": "individual",
+                "staff": "Bob",
+                "patients": ["Francine"],
+            },
+            "Room 3": {
+                "type": None,
+                "group": None,
+                "staff": None,
+                "patients": None,
+            },
+        },
+        "Afternoon": {
+            "Room 1": {
+                "type": "individual",
+                "group": None,
+                "staff": "Alice",
+                "patients": ["Dana"],
+            },
+            "Room 2": {
+                "type": "individual",
+                "staff": "Bob",
+                "patients": ["Eddie"],
+            },
+            "Room 3": {
+                "type": None,
+                "group": None,
+                "staff": None,
+                "patients": None,
+            },
+        },
     },
 }
